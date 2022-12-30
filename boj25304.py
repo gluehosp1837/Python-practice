9eabc59844d885d429c368e07e98574931e1715a
Yejin Kim <59497365+gluehosp1837@users.noreply.github.com>
2022-12-31 00:02:38 +0900

    Add files via upload

---
+++ boj25304.py
@@ -0,0 +1,13 @@
+X = int(input())
+N = int(input())
+
+total = 0
+
+for i in range(N):
+    a, b = map(int, input().split())
+    total += a * b
+
+if total == X:
+    print("Yes")
+else:
+    print("No")
